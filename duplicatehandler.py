6a7a09bb6236cbb04f5b15500ed53de8e72cae3e
Chester <chesta@kekt.us>
2018-10-09 16:08:39 +0200

    improved duplicate accuracy

--- duplicatehandler.py
+++ duplicatehandler.py
@@ -31,8 +31,12 @@ class DuplicateHandler:
         elif img2.shape[0] > img1.shape[0] or img2.shape[1] > img1.shape[1]:
             img2 = img2[0: img1.shape[0], 0: img1.shape[1]]
 
+        img1 = cv2.GaussianBlur(img1, (11, 11), 0)
+        img2 = cv2.GaussianBlur(img2, (11, 11), 0)
+
         diff = cv2.absdiff(img1, img2)
         diff = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
+
         diff = diff.astype(np.uint8)
         # alternatively: (np.sum(diff) / (diff.size * 255)) * 100
         percent = (np.count_nonzero(diff) / diff.size) * 100
